99254f0fe7b9baee993f6f111effbb67f6df1d0c
David Greaves <david@dgreaves.com>
2017-09-14 16:16:03 +0100

    Add support for the alternate rpaf module syntax There are multiple rpaf modules. The one used in Debian is not the one used by Suse. See https://serverfault.com/questions/652279/apache-module-rpaf-documentation for more details.
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- manifests/mod/rpaf.pp
+++ manifests/mod/rpaf.pp
@@ -2,15 +2,28 @@ class apache::mod::rpaf (
   $sethostname = true,
   $proxy_ips   = [ '127.0.0.1' ],
   $header      = 'X-Forwarded-For',
-  $template    = 'apache/mod/rpaf.conf.erb'
+  $template    = 'apache/mod/rpaf.conf.erb',
+  $setport     = true,
+  $forbidifnotproxy = false,
+  $sethttps    = true,
+  $use_stderr_package = true # Compatibility with old/Debian rpaf module
 ) {
   include ::apache
   ::apache::mod { 'rpaf': }
 
-  # Template uses:
+  # There are multiple versions of the rpaf module, see:
+  #    https://serverfault.com/questions/652279/apache-module-rpaf-documentation
+  # The $use_stderr_package parameter is used to select the correct template
+  # if set to 'false' it will use the newer 'gnif' package.
+  # Old/Debian template for the 'stderr' package uses:
   # - $sethostname
   # - $proxy_ips
   # - $header
+  # The newer gnif package also uses
+  # - $setport
+  # - $forbidifnotproxy
+  # - $sethttps
+
   file { 'rpaf.conf':
     ensure  => file,
     path    => "${::apache::mod_dir}/rpaf.conf",
